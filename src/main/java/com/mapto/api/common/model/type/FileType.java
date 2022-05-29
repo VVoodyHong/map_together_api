4650237cb49b9d00bef3255dd98add4613dd60b2
jwhong <hju4287@naver.com>
2022-05-29 20:32:02 +0900

    Add file type enum

---
+++ src/main/java/com/mapto/api/common/model/type/FileType.java
@@ -0,0 +1,32 @@
+package com.mapto.api.common.model.type;
+
+import lombok.Getter;
+
+@Getter
+public enum FileType{
+    //doc type
+    pdf,
+    doc,
+    docx,
+    xls,
+    xlsx,
+    ppt,
+    pptx,
+
+    //image type
+    bmp,
+    jpg,
+    png,
+    gif,
+    ico,
+
+    //video type
+    avi,
+    mpg,
+    mp4,
+    wmv,
+    mov,
+    flv,
+    swf,
+    mkv
+}
