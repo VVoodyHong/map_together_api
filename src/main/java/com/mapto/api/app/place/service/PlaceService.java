9534638f257cdda15bbbb2235a990fb128637aff
jwhong <hju4287@naver.com>
2022-06-06 19:35:10 +0900

    Add multi file upload logic

--- src/main/java/com/mapto/api/app/place/service/PlaceService.java
+++ src/main/java/com/mapto/api/app/place/service/PlaceService.java
@@ -1,8 +1,12 @@
 package com.mapto.api.app.place.service;
 
 import com.mapto.api.app.file.dto.FileDTO;
+import com.mapto.api.app.file.entity.File;
+import com.mapto.api.app.file.repository.FileRepository;
 import com.mapto.api.app.place.dto.PlaceDTO;
 import com.mapto.api.app.place.entity.Place;
+import com.mapto.api.app.place.entity.PlaceFile;
+import com.mapto.api.app.place.repository.PlaceFileRepository;
 import com.mapto.api.app.place.repository.PlaceRepository;
 import com.mapto.api.app.placecategory.entity.PlaceCategory;
 import com.mapto.api.app.placecategory.repository.PlaceCategoryRepository;
@@ -19,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,6 +32,8 @@ public class PlaceService {
     private final PlaceRepository placeRepository;
     private final UserRepository userRepository;
     private final PlaceCategoryRepository placeCategoryRepository;
+    private final PlaceFileRepository placeFileRepository;
+    private final FileRepository fileRepository;
     private final FileUploader fileUploader;
 
     @Transactional
@@ -38,11 +45,7 @@ public class PlaceService {
         } else if(placeInfo.getCategoryIdx() == -1) {
             throw new CustomException(StatusCode.CODE_755);
         }
-        // TODO
-        // N:N 관계 매핑 테이블 생성 후 추가 로직
-//        if(files != null && files.size() != 0) {
-//            fileUploader.upload(files, "place");
-//        }
+
         User user = userRepository.findByIdx(userPrincipal.getUser().getIdx());
         PlaceCategory placeCategory = placeCategoryRepository.findByIdx(placeInfo.getCategoryIdx());
         Place place = Place.builder()
@@ -54,8 +57,29 @@ public class PlaceService {
                 .lat(placeInfo.getLat())
                 .lng(placeInfo.getLng())
                 .build();
-        return placeRepository.save(place).toPlaceBasicDTO();
-    }
+        Place placeEntity = placeRepository.save(place);
 
+        if(files != null && files.size() != 0) {
+            List<FileDTO.Basic> fileList = fileUploader.upload(files, "place");
+            List<File> fileEntities = new ArrayList<>();
+            List<PlaceFile> placeFileEntities = new ArrayList<>();
+            for (FileDTO.Basic basic : fileList) {
+                File file = File.builder()
+                        .name(basic.getName())
+                        .type(basic.getType())
+                        .url(basic.getUrl())
+                        .build();
+                PlaceFile placeFile = PlaceFile.builder()
+                        .place(placeEntity)
+                        .file(file)
+                        .build();
+                fileEntities.add(file);
+                placeFileEntities.add(placeFile);
+            }
+            fileRepository.saveAll(fileEntities);
+            placeFileRepository.saveAll(placeFileEntities);
+        }
+        return placeEntity.toPlaceBasicDTO();
+    }
 }
 
