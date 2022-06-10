e27d03fcdc5a4c6f453f36a5711f05658e8312c6
jwhong <hju4287@naver.com>
2022-06-10 15:19:50 +0900

    Add find by user method

--- src/main/java/com/mapto/api/app/place/repository/PlaceRepository.java
+++ src/main/java/com/mapto/api/app/place/repository/PlaceRepository.java
@@ -1,7 +1,11 @@
 package com.mapto.api.app.place.repository;
 
 import com.mapto.api.app.place.entity.Place;
+import com.mapto.api.app.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PlaceRepository extends CrudRepository<Place, Long> {
+    List<Place> findByUser(User user);
 }
