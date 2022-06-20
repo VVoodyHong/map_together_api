8d4c59a908e815ea50ac41863e387bebc3344771
jwhong <hju4287@naver.com>
2022-06-20 21:00:14 +0900

    Delete find by user idx method

--- src/main/java/com/mapto/api/app/follow/repository/FollowRepositoryCustom.java
+++ src/main/java/com/mapto/api/app/follow/repository/FollowRepositoryCustom.java
@@ -1,11 +1,4 @@
 package com.mapto.api.app.follow.repository;
 
-import com.mapto.api.app.follow.entity.Follow;
-import com.mapto.api.common.model.type.FollowType;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-
 public interface FollowRepositoryCustom {
-    List<Follow> findByUserIdx(Long userIdx, FollowType followType);
 }
